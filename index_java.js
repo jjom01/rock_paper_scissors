86111c37c02ea2dcebfb6d45667caff90883dd25
Juan <juanchosc01@gmail.com>
2026-06-09 18:54:54 -0500

    create method selection for rps but s not working

--- index_java.js
+++ index_java.js
@@ -1,1 +1,16 @@
-console.log("Hello World")
+function getComputerChoice () {
+
+   let num = Math.floor(Math.random() * 9) + 1;
+   if (num <= 3){
+    console.log("rock")
+   }
+   else if (num >= 4 || num <= 5){
+    console.log("paper")
+   }
+   else{
+    console.log("scissors")
+   }
+
+}
+
+getComputerChoice()
