4f29a7c3b92589cbfa6be3857e1a76a808b4b3a8
Juan <juanchosc01@gmail.com>
2026-06-09 19:04:52 -0500

    fix p range to extend till 6

--- index_java.js
+++ index_java.js
@@ -1,10 +1,11 @@
 function getComputerChoice () {
 
-   let num = Math.floor(Math.random() * 9) + 1;
+   let num = Math.floor(Math.random() * 9) + 1; 
+   console.log(num)
    if (num <= 3){
     console.log("rock")
    }
-   else if (num >= 4 && num <= 5){
+   else if (num >= 4 && num <= 6){
     console.log("paper")
    }
    else{
