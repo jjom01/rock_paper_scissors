136b1fa84102e470c0a90ef990dcc24b261660fe
Juan <juanchosc01@gmail.com>
2026-06-14 16:05:10 -0500

    create user input and add case insensitivity

--- index_java.js
+++ index_java.js
@@ -13,5 +13,26 @@ function getComputerChoice () {
    }
 
 }
-
 getComputerChoice()
+
+function getHumanChoice (){
+
+    let hc = prompt("Choose: rock - paper - scissors", "your choice here").toLowerCase();
+
+    if (hc === "rock"){
+        console.log("you picked ROCK")
+    }
+    else if (hc === "paper"){
+        console.log("you picked PAPER")
+    }
+    else if (hc === "scissors")
+        console.log("you picked SCISSORS")
+    else if (hc !== "rock" || hc !== "paper" || hc !== "scissors") {
+        console.log("try a valid option")
+    }
+
+}
+getHumanChoice()
+
+let humanScore = 0
+let computerScore = 0
