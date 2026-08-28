a62589001b8e692fbdeff736ca182f2125377f00
jjom01 <juanchosc01@gmail.com>
2026-08-28 13:22:30 -0500

    add function for computerChoice and log result computer randomly picks R P S and logs both human and CPU choice

--- java.js
+++ java.js
@@ -1,13 +1,34 @@
+function getComputerChoice () {
+    let num = Math.floor(Math.random() * 9) + 1; 
+        if (num <= 3){
+        return("rock")
+        }
+        else if (num >= 4 && num <= 6){
+        return("paper")
+        }
+        else{
+        return("scissors")
+        }
+}
+
 let buttons = document.querySelectorAll('.button');
 
 buttons.forEach(btn =>{
-    btn.addEventListener('click', (event) => {
-        if (event.target.id === 'rock') {
+btn.addEventListener('click', (event) => {
+    if (event.target.id === 'rock') {
+        const computerChoice = getComputerChoice();
+        console.log('Computer chose:', computerChoice);
         console.log('rock was clicked');
-    } else if (event.target.id === 'paper') {
+        } 
+    else if (event.target.id === 'paper') {
+        const computerChoice = getComputerChoice();
+        console.log('Computer chose:', computerChoice);
         console.log('paper was clicked');
-    } else if (event.target.id === 'scissors') {
+        } 
+    else if (event.target.id === 'scissors') {
+        const computerChoice = getComputerChoice();
+        console.log('Computer chose:', computerChoice);
         console.log('scissors was clicked');
-    }
+        }
     })
 })
